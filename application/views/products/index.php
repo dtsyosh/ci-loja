254312ec19d9623a92bf7480b64134e7a38573e6
Diego Tsuyoshi <dts2896@gmail.com>
2017-12-01 20:59:22 -0200

    Criando views para testar o backend

--- application/views/products/index.php
+++ application/views/products/index.php
@@ -1,3 +1,1 @@
-<?= $test ?>
-
 Products index
---
+++ application/views/products/show.php
@@ -0,0 +1,9 @@
+<h2><?= $title ?></h2>
+
+<div class="row">
+    <div class="col s6">
+        <p><?= $product -> name ?></p>
+        <p><?= $product -> price ?></p>
+        <p><?= $product -> quantity ?></p>
+    </div>
+</div>
