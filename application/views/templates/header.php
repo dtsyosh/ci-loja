e4180d80356f365ed351d6ffa88e912148f37290
Diego Tsuyoshi <dts2896@gmail.com>
2017-12-01 18:50:38 -0200

    Adicionando import do Jquery

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     </head>
     <body>
