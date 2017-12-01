d492da4cb785d9150cb0555a8aa4e3e0891d2bd9
Diego Tsuyoshi <dts2896@gmail.com>
2017-12-01 12:41:18 -0200

    Criando arquivos correspondente para produtos

---
+++ application/controllers/ProductsController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* Products Controller
+*/
+class ProductsController extends CI_Controller {
+
+	public function index() {
+		$this -> load -> view('products/index');
+	}
+	
+	public function create() {
+
+	}
+
+	public function update() {
+
+	}
+
+	public function destroy() {
+
+	}
+}
