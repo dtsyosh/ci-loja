aa43fa02e1daec874b7ff3514baabf0fba7d3fa2
Diego Tsuyoshi <dts2896@gmail.com>
2017-12-01 16:22:57 -0200

    Adicionando comentários

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -8,13 +8,21 @@ class Products extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 
+		/*
+		 * Carrega o arquivo model 'Product_model'
+		 * e atribui a ele o apelido 'products'
+		 * para ficar mais facil de chamar
+		 */
+
 		$this -> load -> model('Product_model', 'products');
 	}
 
 	public function index() {
 		
+		// Cria a variavel 'title' na view
 		$data['title'] = 'Produtos';
 
+		// Carrega os arquivos da view: header, pagina, footer
 		$this -> load -> view('templates/header');
 		$this -> load -> view('products/index', $data);
 		$this -> load -> view('templates/footer');
@@ -31,16 +39,19 @@ class Products extends CI_Controller {
 
 	public function store()	{
 		
+		// Invoca o método insert() do model
 		$this -> products -> insert();
 	}
 
 	public function update() {
 		
+		// Invoca o método update() do model
 		$this -> products -> update();
 	}
 
 	public function destroy() {
 
+		// Invoca o método delete() do model
 		$this -> products -> delete();
 	}
 }
--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -4,6 +4,12 @@ class Product_model extends CI_Model {
 
 	public function insert() {
 
+		/*
+		 * Insere no banco através do Query Builder do CI
+		 * Pegando as variaveis de $_POST atraves do método
+		 * $this -> input -> post() do CI pois é mais seguro
+		 */
+
         $this -> db -> insert('products', array(
             'name'          => $this -> input -> post('name'),
 			'price'         => $this -> input -> post('price'),
@@ -12,6 +18,11 @@ class Product_model extends CI_Model {
     }
 
     public function update() {
+		
+		/*
+		 * Atualiza os dados no banco atraves do método
+		 * replace() do Query Builder do CI
+		 */
 		
 		$this -> db -> replace('products', array(
 			'id'       => $this -> input -> post('id'),
